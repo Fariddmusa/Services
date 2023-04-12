f91f6839cce7ee9c71d563ca42d062b8cb453110
Fariddmusa <faridam@code.edu.az>
2023-04-13 03:09:44 +0400

    added files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using BookApp.App.Services;
+
+BookService BookService = new BookService();
+
+Console.WriteLine("0.Close App");
+Console.WriteLine("1.Show Books");
+Console.WriteLine("2.Create Book");
+
+string RequestNumber=Console.ReadLine();
+
+while (RequestNumber != "0")
+{
+    switch (RequestNumber)
+    {
+        case "1":
+            BookService.ShowBooks();
+            break;
+        case "2":
+            BookService.CreateBook();
+            break;
+        default:
+            Console.WriteLine("Select valid option");
+            break;
+    }
+    Console.WriteLine("0.Close App");
+    Console.WriteLine("1.Show Books");
+    Console.WriteLine("2.Create Book");
+
+    RequestNumber = Console.ReadLine();
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using BookApp.Core.Models;
+namespace BookApp.App.Services
+{
+    internal class BookService
+    {
+        private Book[] Books = new Book[0];
+
+        public void ShowBooks()
+        {
+            if(Books.Length == 0)
+            {
+                Console.WriteLine("Not available Books");
+            }
+            foreach(Book book in Books)
+            {
+                book.GetFullInfo();
+            }
+        }
+        public void CreateBook()
+        {
+            Book NewBook= new Book();
+
+            Console.WriteLine("add Name");
+            NewBook.Name=Console.ReadLine();
+            Console.WriteLine("add Price");
+            NewBook.Price = double.Parse(Console.ReadLine());
+
+            Array.Resize(ref Books, Books.Length+1);
+            Books[Books.Length-1] = NewBook;
+        }
+    }
+}
